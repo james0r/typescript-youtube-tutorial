6d2e9ca5a1b15e9aa1cc039f64b4813fc8492a63
James <james.auble@gmail.com>
2023-01-31 15:33:27 -0800

    finished union types

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,10 @@
 "use strict";
-;
-let mySize = 2;
-console.log(mySize);
+function kgToLbs(weight) {
+    if (typeof weight === 'number') {
+        return weight * 2.2;
+    }
+    else {
+        return parseInt(weight) * 2.2;
+    }
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
-// Objects
-let employee: {
-  readonly id: number,
-  name: string,
-  retire: (date: Date) => void
-} = {
-  id: 1,
-  name: 'James',
-  retire: (date: Date) => {
-    console.log(date)
+function kgToLbs(weight: number | string): number {
+  // Narrowing
+  if (typeof weight === 'number') {
+    return weight * 2.2;
+  } else {
+    return parseInt(weight) * 2.2;
   }
-};
+}
 
-employee.id = 5;
