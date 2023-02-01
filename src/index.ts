895538e3ac1b26a277a07955ac388d4ae9f682be
James <james.auble@gmail.com>
2023-01-31 16:02:44 -0800

    completed course

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
-// Nullable types
-function greet(name: string | null | undefined) {
-  if (name) {
-    console.log(name.toUpperCase())
-  } else {
-    console.log('Hola!')
-  }
+type Customer = {
+  birthday?: Date
 }
 
-greet('null')
+function getCustomer(id: number): Customer | null | undefined {
+  return id === 0 ? null : { birthday: new Date() }
+}
+
+let customer = getCustomer(0)
+
+// if (customer !== null) {
+//   console.log(customer.birthday)
+// }
+
+// Optional property access operator
+// Returns null because there is no member property birthday of null
+console.log(customer?.birthday?.getFullYear())
+
+// Optional element access operator
+// customers?.[0]
+
+// Optional call
+let log: any = null
+
+log?.('a')
