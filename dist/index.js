d7655a488f803b2fd97a4f51793f1286384f447d
James <james.auble@gmail.com>
2023-02-01 15:09:10 -0800

    finished Interfaces section

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,2 @@
 "use strict";
-class Shape {
-    constructor(color) {
-        this.color = color;
-    }
-}
-class Circle extends Shape {
-    constructor(radius, color) {
-        super(color);
-        this.radius = radius;
-    }
-    render() {
-        console.log('Rendering a circle');
-    }
-}
-let shape = new Circle(5, 'red');
-shape.render();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,34 @@
-// Abstract Classes and Methods
+// Interfaces
 
-// Abstract classes are blueprints that cannot be instantiated and
-// need to be extended before instantiating
-abstract class Shape {
-  constructor(public color: string) {
+// abstract class Calendar {
+//   constructor(public name: string) { }
 
-  }
+//   abstract addEvent(): void
+//   abstract removeEvent(): void
+// }
 
-  // Child classes must implement this member function (Think contract)
-  abstract render(): void
+// Interfaces might be preferred in the case that an abstract class
+// doesn't provide any logic or algorhythms that can be reused.
+
+interface Calendar {
+  name: string
+  addEvent(): void
+  removeEvent(): void
 }
 
-class Circle extends Shape {
-  constructor(public radius: number, color: string) {
-    super(color)
-  }
-
-  override render(): void {
-      console.log('Rendering a circle')
-  }
+interface CloudCalendar extends Calendar {
+  sync(): void
 }
 
-let shape = new Circle(5, 'red')
+// This is valid TypeScript. If we were to--say--implement CloudCalendar however, we
+// would need to also implement property sync()
+class GoogleCalendar implements Calendar {
+  constructor(public name: string) {}
+  addEvent(): void {
+    throw new Error("Method not implemented.")
+  }
+  removeEvent(): void {
+    throw new Error("Method not implemented.")
+  }
 
-shape.render()
+}
