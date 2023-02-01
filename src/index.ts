08bfff2e95bb05da232a55dacc1632dae4cccaa0
James <james.auble@gmail.com>
2023-01-31 21:06:02 -0800

    finished Index Signatures section

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,14 @@
-// Creating classes
-
-class Account {
-  nickname?: string
-
-  // Here we declare and initialize our variables as arguments for our constructor
-  // These are called Parameter Properties
-  constructor(
-    public readonly id: number,
-    public owner: string,
-    private _balance: number
-  ) {
-    this.id = id
-    this.owner = owner
-    this._balance = _balance
-  }
-
-  deposit(amount: number): void {
-    if (amount <= 0) {
-      throw new Error('Invalid amount')
-    }
-    // Record a transaction
-    this._balance += amount
-    this._balance += this._balance + this.calculateTax()
-  }
-
-  private calculateTax(): number {
-    return this._balance * .091
-  }
-
-  get balance(): number {
-    return this._balance
-  }
+class SeatAssignment {
+  // A1, A2, ...
+  // Index signature property
+  [seatNumber: string]: string
 }
 
-let account = new Account(1, 'Mosh', 0)
-account.deposit(100)
+let seats = new SeatAssignment()
 
-console.log(account.balance)
+// Good
+seats.A1 = 'James'
+seats.A2 = 'Roger'
+
+// Bad
+seats.A3 = 25
