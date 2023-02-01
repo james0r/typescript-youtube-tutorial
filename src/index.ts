65f6f56e237ee6291c95adb151895f69d873e819
James <james.auble@gmail.com>
2023-01-31 17:36:28 -0800

    finished the unknown type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-let phone = <HTMLInputElement> document.getElementById('phone')
-// HTMLElement
-// HTMLInputElement
-console.log(phone.value)
+function render(document: unknown) {
+  // Type unknown is better than type any because it requires that we type check member funcs & vars
+  // Narrowing
+  if (typeof document === 'string') {
+    document.move()
+    document.fly()
+  }
+}
