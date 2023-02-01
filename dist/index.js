c01fedd2b58695b30f3ed5ddee5c850b5c3ee139
James <james.auble@gmail.com>
2023-01-31 20:52:41 -0800

    finished Access Control Keywords

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,25 @@
 "use strict";
 class Account {
-    constructor(id, owner, balance) {
+    constructor(id, owner, _balance) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = _balance;
     }
     deposit(amount) {
         if (amount <= 0) {
             throw new Error('Invalid amount');
         }
-        this.balance += amount;
+        this._balance += amount;
+        this._balance += this._balance + this.calculateTax();
+    }
+    calculateTax() {
+        return this._balance * .091;
+    }
+    getBalance() {
+        return this._balance;
     }
 }
 let account = new Account(1, 'Mosh', 0);
 account.deposit(100);
-console.log(account instanceof Account);
+console.log(account.getBalance());
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -4,26 +4,34 @@ class Account {
   // The below properties only exist in TypeScript, not in Javascript.
   readonly id: number
   owner: string
-  balance: number
+  private _balance: number
   nickname?: string
 
-  constructor(id: number, owner: string, balance: number) {
+  constructor(id: number, owner: string, _balance: number) {
     this.id = id
     this.owner = owner
-    this.balance = balance
+    this._balance = _balance
   }
 
   deposit(amount: number): void {
     if (amount <= 0) {
       throw new Error('Invalid amount')
     }
+    // Record a transaction
+    this._balance += amount
+    this._balance += this._balance + this.calculateTax()
+  }
 
-    this.balance += amount
+  private calculateTax():number {
+    return this._balance * .091
+  }
+
+  getBalance(): number {
+    return this._balance
   }
 }
 
 let account = new Account(1, 'Mosh', 0)
 account.deposit(100)
 
-// Because typeof will just return object we need to use instanceof to compare against class type
-console.log(account instanceof Account)
+console.log(account.getBalance())
