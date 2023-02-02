2370e196c9b64ae23cd5a6058ba6943c052c1146
James <james.auble@gmail.com>
2023-02-01 21:38:08 -0800

    finished Generic Interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
-// Generic Functions
+// https://mywebsite.com/users
+// https://mywebsite.com/products
 
-class ArrayUtils {
-  
-  static wrapInArray<T>(value: T) {
-    return [value]
-  }
+// James's explanation:
+// Generics allow us to "prop drill" types through in this case
+// first a function invocation, through the function definition, through an interface type to a property
+// type union. Crazy right? A take away is the flexibility and polymorphyism of this approach that
+// allows us to dynamically change the shape of our fetch() return type without making changes to the actual
+// function definition. Like "open for extension and closed for modification" from the polymorphyism section.
+
+interface Result<T> {
+  data: T | null,
+  error: string | null
 }
 
-let numbers = ArrayUtils.wrapInArray(1)
+function fetch<T>(url: string): Result<T> {
+  return { data: null, error: null }
+}
+
+interface User {
+  username: string
+}
+
+interface Product {
+  title: string
+}
+
+let result = fetch<User>('url')
+result.data
