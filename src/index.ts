166cb7e90d6a39d0dc1fd823835766fed9f4005d
James <james.auble@gmail.com>
2023-02-01 21:18:26 -0800

    finished Generic Classes

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,9 @@
-// Interfaces
+// Generics Classes
 
-// abstract class Calendar {
-//   constructor(public name: string) { }
+class KeyValuePair<K, V> {
+  constructor(public key: K, public value: V) {
 
-//   abstract addEvent(): void
-//   abstract removeEvent(): void
-// }
-
-// Interfaces might be preferred in the case that an abstract class
-// doesn't provide any logic or algorhythms that can be reused.
-
-interface Calendar {
-  name: string
-  addEvent(): void
-  removeEvent(): void
-}
-
-interface CloudCalendar extends Calendar {
-  sync(): void
-}
-
-// This is valid TypeScript. If we were to--say--implement CloudCalendar however, we
-// would need to also implement property sync()
-class GoogleCalendar implements Calendar {
-  constructor(public name: string) {}
-  addEvent(): void {
-    throw new Error("Method not implemented.")
   }
-  removeEvent(): void {
-    throw new Error("Method not implemented.")
-  }
-
 }
+
+let pair = new KeyValuePair<string, string>('1', 'a')
