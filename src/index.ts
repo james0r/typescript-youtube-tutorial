e4439d346096d59778069d2b4b48a6374936a721
James <james.auble@gmail.com>
2023-02-02 18:13:37 -0800

    finished Class Decorators section

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,28 @@
-// Type Mapping
+// Class Decorators
 
-interface Product {
-  name: string
-  price: number
+// Another way to achieve our decorator example using a base class
+// class Component {
+//   insertInDOM() {}
+// }
+
+// class ProfileComponent extends Component {
+
+// }
+
+// PascalCase
+function Component(constructor: Function) {
+  console.log('Component decorator called')
+  constructor.prototype.uniqueId = Date.now()
+  constructor.prototype.insertInDOM = () => {
+    console.log('Inserting the component in the DOM')
+  }
 }
 
-type ReadOnlyProduct = {
-  // Index signature
-  // keyof
-  readonly [Property in keyof Product]: Product[Property]
+@Component
+class ProfileComponent {
+  constructor(name: string) {
+    console.log(name)
+  }
 }
 
-let product: ReadOnlyProduct = {
-  name: 'a',
-  price: 1
-}
-
-// ...and using Generics
-type ReadOnly<T> = {
-  readonly [K in keyof T]: T[K]
-}
-
-let roProduct: ReadOnly<Product> = {
-  name: 'a',
-  price: 1
-}
-
-type Optional<T> = {
-  [K in keyof T]?: T[K]
-}
-
-// No error here because properties are all optional
-let opProduct: Optional<Product> = {
-  name: 'a'
-}
-
-// The above examples are common and thus included in TypeScript referred to as Utility Types
-// https://www.typescriptlang.org/docs/handbook/utility-types.html
+let profile = new ProfileComponent('James')
