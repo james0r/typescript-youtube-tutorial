25e7a904da98e3110afca28208ead7d6b3a6efe9
James <james.auble@gmail.com>
2023-02-03 15:55:41 -0800

    finished Accessor Decorators section

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,23 @@
-// Method Decorators
-// https://www.typescriptlang.org/docs/handbook/decorators.html#method-decorators
+// Accessor Decorators
 
-function Log(target: any, methodName: string, descriptor: PropertyDescriptor) {
-  const original = descriptor.value as Function
-  descriptor.value = function (...args: any) {
-    console.log('Before')
-    original.call(this, ...args)
-    console.log('After')
+function Capitalize(target: any, methodName: string, descriptor: PropertyDescriptor) {
+  const original = descriptor.get
+  descriptor.get = function() {
+    const result = original!.call(this)
+    return (typeof result === 'string') ? result.toUpperCase() : result
   }
 }
 
 class Person {
-  @Log
-  say(message: string) {
-    console.log('Person says ' + message)
+  constructor(public firstName: string, public lastName: string) {
+
+  }
+
+  @Capitalize
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`
   }
 }
 
-let person = new Person()
-person.say('Hello')
+let person = new Person('James', 'Auble')
+console.log(person.fullName)
